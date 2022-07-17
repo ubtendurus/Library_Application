98704a0f9a77b164422c1098e4c4b01c63740e31
Ugur Tendurus <utendurus@gmail.com>
2022-07-17 04:10:27 -0400

    Test Home Page added to Application

--- src/main/java/com/example/Library_Application/LibraryApplication.java
+++ src/main/java/com/example/Library_Application/LibraryApplication.java
@@ -9,9 +9,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class LibraryApplication {
+
+	@RequestMapping("/")
+	@RequestBody
+	String home(){
+		return "Test Home Page";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
